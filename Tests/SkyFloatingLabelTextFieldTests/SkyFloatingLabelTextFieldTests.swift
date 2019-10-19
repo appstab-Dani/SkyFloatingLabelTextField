65bb84b53c87baf2e65f45a0fc86a958e295c0a3
Dani Olivan <dani.olivan@appstablishment.com>
2019-10-19 20:12:24 +0800

    commenting the test

--- Tests/SkyFloatingLabelTextFieldTests/SkyFloatingLabelTextFieldTests.swift
+++ Tests/SkyFloatingLabelTextFieldTests/SkyFloatingLabelTextFieldTests.swift
@@ -6,7 +6,7 @@ final class SkyFloatingLabelTextFieldTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SkyFloatingLabelTextField().text, "Hello, World!")
+//        XCTAssertEqual(SkyFloatingLabelTextField().text, "Hello, World!")
     }
 
     static var allTests = [
