9bc6dd050d662422a924ff60a09cc2cd3c4e3de7
Dani Olivan <dani.olivan@appstablishment.com>
2019-10-19 20:07:39 +0800

    updated to SPM

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SkyFloatingLabelTextFieldTests
+
+var tests = [XCTestCaseEntry]()
+tests += SkyFloatingLabelTextFieldTests.allTests()
+XCTMain(tests)
---
+++ Tests/SkyFloatingLabelTextFieldTests/SkyFloatingLabelTextFieldTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import SkyFloatingLabelTextField
+
+final class SkyFloatingLabelTextFieldTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SkyFloatingLabelTextField().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
